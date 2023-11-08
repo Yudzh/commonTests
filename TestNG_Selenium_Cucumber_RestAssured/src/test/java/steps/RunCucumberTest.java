efdb77e9a59757dc2230c7bc58ba9fd41872908a
e.pavlov <e.pavlov@unlimit.com>
2023-11-09 01:13:57 +0700

    Added allure

--- TestNG_Selenium_Cucumber_RestAssured/src/test/java/steps/RunCucumberTest.java
+++ TestNG_Selenium_Cucumber_RestAssured/src/test/java/steps/RunCucumberTest.java
@@ -5,14 +5,17 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "steps"
+    features = "src/test/resources/features",
+    glue = "steps",
+    plugin = {
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    }
 )
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
-    @DataProvider(parallel = true)
-    @Override
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+  @DataProvider(parallel = true)
+  @Override
+  public Object[][] scenarios() {
+    return super.scenarios();
+  }
 }
