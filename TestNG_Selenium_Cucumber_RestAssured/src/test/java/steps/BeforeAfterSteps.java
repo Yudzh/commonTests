c1233a97db968d244e62c8d30f7dfea171428037
e.pavlov <e.pavlov@unlimit.com>
2023-11-11 13:47:53 +0700

    Added screenshot

--- TestNG_Selenium_Cucumber_RestAssured/src/test/java/steps/BeforeAfterSteps.java
+++ TestNG_Selenium_Cucumber_RestAssured/src/test/java/steps/BeforeAfterSteps.java
@@ -3,7 +3,10 @@ package steps;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.qameta.allure.Attachment;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 @Slf4j
 public class BeforeAfterSteps extends BaseSteps{
@@ -18,8 +21,14 @@ public class BeforeAfterSteps extends BaseSteps{
     public void close(Scenario scenario){
         log.info("Scenarion " + scenario.getName() + " end");
         if(driver.get() != null){
+            screenshot();
             driver.get().quit();
             driver.remove();
         }
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] screenshot() {
+        return ((TakesScreenshot) driver.get()).getScreenshotAs(OutputType.BYTES);
     }
 }
